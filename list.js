9e56ef19e2cded3a1a00978c9d76d57c942af4fd
cynthiaarubia <cynthiaarubia2002@gmail.com>
2024-12-08 16:44:28 +0300

    first commit

---
+++ list.js
@@ -0,0 +1,31 @@
+let myItems= [];
+const itemEl= document.getElementById("item-el");
+const submitEl=document.getElementById("submit-el");
+const clearListEl=document.getElementById("clearlist-el");
+
+const myListEl=document.getElementById("myList-el");
+
+submitEl.addEventListener("click",function(){
+
+    myItems.push(itemEl.value);
+    itemEl.value="";
+    localStorage.setItem("myItems", JSON.stringify(myItems));
+    return rendermyItems();
+})
+
+
+function rendermyItems(){
+    let listItems="";
+    for (i=0; i<myItems.length; i++){
+        console.log(myItems[i]);
+
+        // myListEl.textContent+= myItems[i];
+
+        myListEl.innerHTML+= "<li>" +myItems[i]+ "</li>"
+        
+        listItems+= "<li>" +myItems[i]+ "</li>"    
+    }
+     
+    myListEl.innerHTML= listItems;
+}
+
